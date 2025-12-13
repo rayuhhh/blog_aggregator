9380d435d49014119a580bc350badb11a099b288
rayuhhh <rayuhhh@gmail.com>
2025-12-13 14:03:52 -0600

    refactoring, added delete users table and reset db func

--- src/lib/db/queries/users.ts
+++ src/lib/db/queries/users.ts
@@ -8,7 +8,16 @@ export async function createUser(name: string) {
   return result;
 }
 
+
 export async function getUser(name: string) {
     const result = await db.select().from(users).where(eq(users.name, name));
     return firstOrUndefined(result);
+}
+
+export async function deleteUsers() {
+  await db.delete(users);
+}
+
+export async function getUsers() {
+  return db.select().from(users);
 }
